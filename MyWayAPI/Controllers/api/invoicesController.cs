e97bef6df060ece3f359d350e4547d6a9812f17c
abayman <abdelrahmanayman48@gmail.com>
2018-08-16 16:18:57 +0300

    new

--- MyWayAPI/Controllers/api/invoicesController.cs
+++ MyWayAPI/Controllers/api/invoicesController.cs
@@ -7,27 +7,27 @@ using System.Web.Http;
 
 namespace MyWayAPI.Controllers.api
 {
-    public class pendingController : ApiController
+    public class invoicesController : ApiController
     {
-        [Route("api/pending/{id}")]
+        [Route("api/invoices/{id}")]
         [HttpGet]
-        public IHttpActionResult getpendingsalesorder (string id)
+        public IHttpActionResult getinvoices (string id)
         {
-            List<PENDING_Result> pendinglist = new List<PENDING_Result>();
-            var test = new Entities1();
-            var reslist = test.PENDING(id).ToList();
-            foreach(var pen in reslist.ToList())
+            List<INVOICE_Result> pendinglist = new List<INVOICE_Result>();
+            var test = new Entities2();
+            var reslist = test.INVOICE(id).ToList();
+            foreach (var pen in reslist.ToList())
             {
                 pendinglist.Add(
-                    new PENDING_Result
+                    new INVOICE_Result
                     {
                         DOC_ID = pen.DOC_ID,
                         ITEM_ID = pen.ITEM_ID,
                         ANAME = pen.ANAME,
                         COUNTER = pen.COUNTER,
-                        QTY_REQ = pen.QTY_REQ,
-                        UNIT_PRICE = pen.UNIT_PRICE,
-                        TOT_PRICE = pen.TOT_PRICE,
+                        QTY = pen.QTY,
+                        PRICE = pen.PRICE,
+                        NET_TOTAL = pen.NET_TOTAL,
                         ITEM_BP = pen.ITEM_BP,
                         TOTAL_BP = pen.TOTAL_BP,
                         DOC_DATE = pen.DOC_DATE
