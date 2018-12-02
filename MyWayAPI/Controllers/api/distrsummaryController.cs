a51efcdc34b1c436fc1740eb33747b71911251fb
abayman <abdelrahmanayman48@gmail.com>
2018-12-02 20:21:12 +0200

    Updated apis, new apis

---
+++ MyWayAPI/Controllers/api/distrsummaryController.cs
@@ -0,0 +1,39 @@
+﻿using MyWayAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace MyWayAPI.Controllers.api
+{
+    public class distrsummaryController: ApiController
+    {
+        [Route("api/distrsummary/{id}")]
+        [HttpGet]
+        public IHttpActionResult getsummary(string id)
+        {
+            List<distr_summary_Result> x = new List<distr_summary_Result>();
+            var test = new Entities5();
+            var reslist = test.distr_summary(id).ToList();
+            foreach (var pen in reslist.ToList())
+            {
+                x.Add(
+                    new distr_summary_Result
+                    {
+                        DISTR_ID = pen.DISTR_ID,
+                        NAME = pen.NAME,
+                        sponser_id = pen.sponser_id,
+                        sponser_name = pen.sponser_name,
+                        area = pen.area,
+                        count = pen.count,
+                        PER_BP = pen.PER_BP,
+                        RATIO = pen.RATIO,
+                        //Group_bp = pen.Group_bp,
+                        SUM = pen.SUM                  
+                    });
+            }
+            return Ok(x);
+        }
+    }
+}
---
+++ MyWayAPI/Controllers/api/leaderverificationController.cs
@@ -0,0 +1,31 @@
+﻿using MyWayAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using MyWayAPI.Models;
+
+namespace MyWayAPI.Controllers.api
+{
+    public class leaderverificationController : ApiController
+    {
+         private readonly DBC _ctx;
+         public leaderverificationController()
+        {
+            _ctx = new DBC();
+        }
+
+        [Route("api/leaderverification/{leaderID}/{distrID}")]
+        [HttpGet]
+        public IHttpActionResult verifyLeader (string leaderID, string distrID)
+         {
+             var test = new verification();
+
+            var x = test.LEADER_verification(leaderID, distrID);
+
+            return Ok(x);
+         }
+        
+    }
+}
